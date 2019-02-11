7868efb49e962eb68accba8be8a9e12f6cf3f038
Roger <luojie@w-ew.com>
2019-02-11 08:43:05 +0800

    增加doc

--- springboot/src/main/java/com/springboot/service/impl/UserService.java
+++ springboot/src/main/java/com/springboot/service/impl/UserService.java
@@ -1,9 +1,6 @@
 package com.springboot.service.impl;
 
-import java.util.Collection;
 import java.util.List;
-import java.util.Map;
-import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
